0238a37db5c1d91367701514cce0e484d1dbe85f
junmin-yoo <sx0819@gmail.com>
2020-09-08 11:14:16 +0900

    201740224 유준민

---
+++ 201740224/hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        echo "코드실행1. 대림대학교";
+    ?>
+
+    <hr>
+
+    <?php
+        echo "코드실행2. 반갑습니다.";
+    ?>
+
+</body>
+</html>
