88eaeb80509681cdf5b76733b0a36d968cb74bdf
bp4sp4 <bp4sp4@naver.com>
2020-09-08 15:34:12 +0900

    박상훈-echo 출력 실습

---
+++ 201740116/hello3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "대림대학교";
+    ?>
+
+    <hr>
+
+    <?php
+        print "201740116 박상훈입니다";
+    ?>
+
+</body>
+</html>
