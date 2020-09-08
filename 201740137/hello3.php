f2bf8e198b7be245ca3f3bbbfeffb603d1abd88b
wnsgudchl0302 <wnsgudchl0302@naver.com>
2020-09-08 15:34:18 +0900

    이호진 -echo 출력 실습

---
+++ 201740137/hello3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "대림대학교";
+    ?>
+
+    <hr>
+    
+    <?php
+    echo "201740137 학번 최준형 입니다.";
+    ?>
+</body>
+</html>
