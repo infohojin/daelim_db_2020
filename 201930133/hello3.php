599a2c914853329e4d6755f55868b08d05075c89
YeEun-Choi <haim221022@gmail.com>
2020-09-15 00:05:49 +0900

    최예은-hello3.php

---
+++ 201930133/hello3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        echo "대림대학교";
+    ?>
+    <?php
+        echo "201930133 학번 최예은입니다.";
+    ?>
+
+</body>
+</html>
