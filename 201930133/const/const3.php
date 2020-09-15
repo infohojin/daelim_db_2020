aec187b28f19749138698b2b4dfa37c78841dc9d
YeEun-Choi <haim221022@gmail.com>
2020-09-15 10:35:54 +0900

    최예은 - const 폴더

---
+++ 201930133/const/const3.php
@@ -0,0 +1,13 @@
+<?php
+
+    const DAELIM = "대림 univ\n";
+    echo DAELIM;
+    echo DAELIM;
+    echo DAELIM;
+    echo DAELIM;
+    echo DAELIM;
+    echo DAELIM;
+    echo DAELIM;
+    echo DAELIM;
+
+?>
