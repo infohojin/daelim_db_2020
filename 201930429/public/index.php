bc6611769a167ae593ee11f4549eda48302cc816
GamJarin <59593043+JangIBin@users.noreply.github.com>
2020-09-22 12:49:40 +0900

    09월 20일 수업내용 커밋, 5주차 퀴즈

---
+++ 201930429/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    // 함수를 호출해서 사용꺼야
+    // 값을 전달해주면, 문자열 전달할꺼야. 파일주소
+    $filename = "../"; // 상위폴더(학번)
+    $filename .= "resource/layout.html"; // 복합연산자
+    //. 문자열을 더하는 연산자
+    // =대입
+
+    // file_get_contents 함수는 
+    // 파일을 읽어서 내용을 결과값으로 반환
+    $body = file_get_contents($filename);
+    $header = file_get_contents("../resource/header.html");
+
+    // body에 str_replce 함수의 결과값을 더합
+    $body = str_replace("{(header)}", $header, $body);
+
+    $footer = file_get_contents("../resource/footer.html");
+    $body = str_replace("<<푸터>>", $footer, $body);
+
+    echo $body;
+
+?>
