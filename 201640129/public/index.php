2ce135a73e396b419fe34ec21118ba28970da2b6
leedongjin1234 <dhejrh@daum.net>
2020-09-22 16:48:40 +0900

    이동진-오류수정 누락 3.4주차 퀴즈와 수업내용

---
+++ 201640129/public/index.php
@@ -0,0 +1,26 @@
+<?php
+//echo "여기가 시작이얏~~~~~~~~~~~~~~";
+
+$filename = "../";
+$filename .= "resource/layout.html";
+
+echo $fimename;
+
+$body = file_get_contents($filename);
+
+$header = file_get_contents("../resource/header.html");
+//echo $header;
+$body = str_replace("{{header}}",$header,$body);
+
+$footer = file_get_contents("../resource/footer.html");
+$body = str_replace("{{footer}}",$footer,$body);
+
+
+
+$main = file_get_contents("../resource/main.html");
+$body = str_replace("{{main}}",$main,$body);
+
+
+
+
+echo $body;
