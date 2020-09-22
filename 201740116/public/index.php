9fdbcb4d4c8e133375a2f46ce49fb46b37f8d2f4
bp4sp4 <bp4sp4@naver.com>
2020-09-22 17:17:22 +0900

    0922오늘 수업  5주자퀴즈 수정

--- 201740116/public/index.php
+++ 201740116/public/index.php
@@ -1,27 +1,29 @@
 <?php
-//echo "여기가 시작이야....";
+#echo "여기가 시작이야아아아아";
 $filename = "../";
 $filename .= "resource/layout.html";
 
-//echo $filename;
-//함수
+#echo $filename;
+
 $body = file_get_contents($filename);
 
 $header = file_get_contents("../resource/header.html");
-#echo $header;
+
 $body = str_replace("{{header}}",$header,$body);
 
 $footer = file_get_contents("../resource/footer.html");
 $body = str_replace("{{footer}}",$footer,$body);
 
-function footer($filename, $body)
-{
-$footer = file_get_contents($filename);
-$body = str_replace("{{main}}",$footer,$body);
 
-return $body;
+function main($filename, $body)
+{
+    $footer = file_get_contents($filename);
+    $body = str_replace("{{main}}",$footer,$body);
+
+    return $body;
 }
 
-$body = footer("../resource/main.html", $body);
+$body = main("../resource/main.html",$body);
 
-echo $body;
+
+echo $body; 
