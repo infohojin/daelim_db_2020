2610b92cee101765bcb084608679e933f79cda8e
woosub <woosub0602@naver.com>
2020-09-22 16:43:27 +0900

    09_22 수업, 함수선언

---
+++ 201640108/public/index.php
@@ -0,0 +1,29 @@
+<?php
+#echo "여기가 시작이야아아아아";
+$filename = "../";
+$filename .= "resource/layout.html";
+
+#echo $filename;
+
+$body = file_get_contents($filename);
+
+$header = file_get_contents("../resource/header.html");
+
+$body = str_replace("{{header}}",$header,$body);
+
+$footer = file_get_contents("../resource/footer.html");
+$body = str_replace("{{footer}}",$footer,$body);
+
+
+function main($filename, $body)
+{
+    $footer = file_get_contents($filename);
+    $body = str_replace("{{main}}",$footer,$body);
+
+    return $body;
+}
+
+$body = main("../resource/main.html",$body);
+
+
+echo $body;
