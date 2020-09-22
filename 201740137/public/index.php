203ecd6e66dccd35ddf46a834d3ba75bcee363f4
wnsgudchl0302 <wnsgudchl0302@naver.com>
2020-09-22 16:42:20 +0900

    자기이름-메시지 작성

---
+++ 201740137/public/index.php
@@ -0,0 +1,32 @@
+<?php
+//echo "여기가 시작이얏~~~~~~~~~~~~~~";
+
+$filename = "../";
+$filename .= "resource/layout.html";
+
+echo $fimename;
+
+$body = file_get_contents($filename);
+
+$header = file_get_contents("../resource/header.html");
+//echo $header;
+$body = str_replace("{{header}}",$header,$body);
+
+$footer = file_get_contents("../resource/footer.html");
+$body = str_replace("{{footer}}",$footer,$body);
+
+
+function main($filename, $body)
+{
+    $main = file_get_contents($filename);
+    $body = str_replace("{{main}}",$main,$body);
+
+    return $body;
+}
+$body = main("../resource/main.html",$body);
+
+
+
+echo $body;
+
+
