46d739214304542ef75f389e625e594e6f53be99
rx460 <rx460@naver.com>
2020-09-22 16:41:43 +0900

    09-22 수업, 함수 선언까지

---
+++ 201740238/op/alu.php
@@ -0,0 +1,34 @@
+<?php
+echo "더하기 연산을 합니다<hr>";
+echo "<br>";
+echo 1+2+3;
+echo "<br>";
+$a = 10;
+$b = 3.14;
+echo $a + $b;
+echo "<br>";
+echo "빼기 연산을 합니다<hr>";
+echo 3 -2;
+echo 3+ (-2);
+
+
+echo "<br>";
+echo "증가 연산을 합니다<hr>";
+$c =1;
+echo $c;
+$c  = $c + 1;
+echo $c;
+$c  = $c + 1;
+echo $c;
+$c  = $c + 1;
+echo $c;
+
+$c++;
+echo $c;
+
+echo $c++;
+echo $c;
+
+echo --$c;
+echo --$c;
+echo --$c;
