fac6d297b533f3f722e2021e423196dfeba12d3d
scian0204 <scian0204@gmail.com>
2020-09-29 12:48:53 +0900

    201930107 남궁찬 6주차 실습

---
+++ 201930107/func/func07.php
@@ -0,0 +1,19 @@
+<?php
+if (function_exists("hello")) {
+    // echo "hello함수 존재";
+} else {
+    // echo "hello함수 없음";
+    
+    // 조건 제어문 코드블럭 안에서도 함수 선언 가능
+    function hello($name="초기값")
+    {
+        echo '안녕하세요.<br>';
+        print '대림대학교의 ';
+        print "$name 입니다.<br>";
+        return true;
+    }
+}
+
+hello();
+hello("홍길동");
+hello("홍길동");
