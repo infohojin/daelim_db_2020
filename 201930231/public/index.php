5109bb589d6225520441acf55bd536a0072236d9
unknown <cathy010673@gmail.com>
2020-09-29 17:25:48 +0900

    201930231 전소진 0929_실습

--- 201930231/public/index.php
+++ 201930231/public/index.php
@@ -1,34 +1,21 @@
 <?php
-// 소스 코드
-// MVC -> C(Controller)
-# echo "여기가 시작이야~!";
+include "resource.php";
+include "resource.php";
 
-$filename = "../";  // 상위 폴더
-$filename .= "resource/layout.html";
+$theme = "layout04";
+// $body = html_get_contents($theme);
 
-# echo $filename;
-// 함수
-$body = file_get_contents("$filename");  // 함수의 값을 대입, 파일을 읽어서 값을 반환함
-// 전달받은 문자열과 동일한 파일을 찾아서 읽어서 반환한다.
+$color1 = "#0277bd";  // 초기값
+$color2 = "#512da8";  // 초기값2
 
-$header = file_get_contents("../resource/header.html"); 
-# echo $header;
-$body = str_replace("{{header}}", $header, $body);  // 문자열을 찾아서 치환해줌
+$vars = ['header_color' => $color2];
 
-$footer = file_get_contents("../resource/footer.html");
-$body = str_replace("{{footer}}", $footer, $body);
+$filename = "../resource/layout02/layout.html";
+$body = resource($filename, $vars);
 
-// 함수 선언 
-function main($filename, $body)  // 두 개의 값을 받음
-{
-    // 코드를 함수로 만들기
-    // 코드를 묶어주어야 함
-    $main = file_get_contents($filename);
-    $body = str_replace("{{main}}", $main, $body);
+$content = html_get_contents($theme, "hello.html");
 
-    return $body;
-}
-// 함수 사용
-$body = main("../resource/main.html", $body);
+// 문자열 치환
+$body = str_replace("{{contents}}", $content, $body);
 
 echo $body;
