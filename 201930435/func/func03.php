2c3051fa616b8435ff7bf09403af92f19dfd6983
yedogwalwal <zxcv12045@naver.com>
2020-09-29 16:39:54 +0900

    홍예연-6주차 실습

---
+++ 201930435/func/func03.php
@@ -0,0 +1,17 @@
+<?php
+
+$name1 = hello("대림이");
+echo $name1;
+echo hello("대숙이");
+
+print hello();
+
+function hello($name="홍예연")
+{
+  $text = "";
+  $text .= "안녕하세요." . "<br>";
+  $text .= "날씨가 참 좋네요." . "<br>";
+  $text .= "저는 대림대학교 <b>$name</b> 입니다.<br>";
+
+  return $text;
+}
