858e7f92c298e0bd97502ed5aca29afa504d7a52
GamJarin <59593043+JangIBin@users.noreply.github.com>
2020-09-29 12:49:28 +0900

    0929 수업내용, 6주차 퀴즈

---
+++ 201930429/func/func01.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "안녕하세요.<br>";
+    print "대림대학교의";
+    print "대숙이 입니다." . "<br>";
+
+    echo "안녕하세요.<br>";
+    print "대림대학교의";
+    print "대숙이 입니다.<br>";
+
+    echo "안녕하세요.<br>";
+    print "대림대학교의";
+    print "대숙이 입니다.<br>";
+
+?>
