ae55e7ddcbc1e4d817b288827ed3092ed2ff5769
namkoun <skarud0328@gmail.com>
2020-10-06 15:01:17 +0900

    김남경-7주차 수업,퀴즈

---
+++ 201740202/loop/loop3.php
@@ -0,0 +1,10 @@
+<?php
+// $msg = "abcde";
+// echo $msg;
+
+$fruit = ['apple'=>"사과",'grape'=>"포도",'con'=>"옥수수"];
+foreach($fruit as $key => $value){
+    echo $key."=>";
+    echo $value;
+    print "<br>";
+}
