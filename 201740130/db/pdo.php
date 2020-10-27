a9b3ca04c639c033cf732eab9a919ec7a19efed6
skullax9 <skullax9@naver.com>
2020-10-27 12:50:08 +0900

    이영찬 10주차 실습 및 퀴즈 작성

---
+++ 201740130/db/pdo.php
@@ -0,0 +1,14 @@
+<?php
+include "../dbinfo.php";
+
+try {
+    $db = new PDO("mysql:host=localhost;dbname=phpdaelim5;",
+    "daelim5","0000");
+    if($db) {
+        echo "DB접속 성공";
+    } else {
+        echo "DB접속 실패";
+    }
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
