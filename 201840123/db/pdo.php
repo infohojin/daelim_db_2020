2dbd98c7370ec040ca67fc387f06434dc6cc0fc5
ost960102 <ostaxi1996@gmail.com>
2020-10-27 17:00:21 +0900

    오승택-1027

---
+++ 201840123/db/pdo.php
@@ -0,0 +1,17 @@
+<?php
+include "../dbinfo.php";
+
+try{
+    //객체를 생성해줘.
+    $db = new PDO(
+        "mysql:host=".$dbinfo['master']['dbhost'].";".$dbname=$dbinfo['master']['dbschema'].";",$dbinfo['master']['dbuser'],$dbinfo['master']['dbpass']
+    );
+    if($db){
+        echo "DB접속 성공";
+    }else{
+        echo "DB접속 실패";
+    }
+}catch(PDOException $e){
+    //생성하다가 오류
+    echo $e->getMessage();
+}
---
+++ 201840123/dbinfo.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "daelim4";
+$dbpass = "123456";
+$dbshema = "phpdaelim4";
+
+echo "DB접속 정보 입니다.";
