832d0ab1e823f9be0a10319ff1f804a7463f5037
YeEun-Choi <haim221022@gmail.com>
2020-10-27 21:36:58 +0900

    최예은 - 10주차 수업&퀴즈

--- 123456/db/index.php
+++ 123456/db/index.php
@@ -3,11 +3,11 @@
 // () <== 값을 전달해 주는 역할
 // 객체를 생성을 하는데...(개체를 만드는 과정중,..)
 // 인자값을 전달...
-$db = new mysqli("localhost","daelim5","123456","phpdaemin5");
+$db = new mysqli("localhost","daelim5","1234","phpdaelim5");
 if ($db) {
     echo "DB 접속 성공"."<br>";
     
-    $query = "SELECT * FROM phpdaemin5.members;"; // SQL 쿼리문
+    $query = "SELECT * FROM phpdaelim5.members;"; // SQL 쿼리문
     $result = mysqli_query($db, $query); // DB서버로 전송
     if ($result) {
         $row = mysqli_fetch_object($result);
--- 201930133/db/index.php
+++ 201930133/db/index.php
@@ -1,18 +1,67 @@
 <?php
-    // 객체 생성 
-    // () > 값을 전달해주는 역할
-    // 인자값 전달..
-    $db = new mysqli("localhost","dealim5","1234","phpdaelim5");
-    if ($db->connect_error){
-        echo "db 접속 성공"."<br>";
-        $query = "select * from phpdaelim5.members;"; //sql 쿼리문
-        $result = mysqli_query($db, $query); // db 서버로 전송
-        if($result){
-            $row = mysqli_fetch_object($result);
-            print_r($row);
-        }else{
-            echo "데이터 읽기 실패";
-        }
-    } else{
-        echo "db 접속 실패";
+// 객체를 생성 
+// () <== 값을 전달해 주는 역할
+// 객체를 생성을 하는데...(개체를 만드는 과정중,..)
+// 인자값을 전달...
+
+include "../dbinfo.php";
+
+$db0 = new mysqli(
+    $dbinfo['master']['dbJost'],
+    $dbinfo['master']['dbUser'],           
+    $dbinfo['master']['dbPwd'],
+    $dbinfo['master']['dbSchema']
+);
+if ($db0) {
+    echo "DB 접속 성공"."<br>";
+
+    $query = "SELECT * FROM phpdaelim5.members;"; 
+    $result = mysqli_query($db0, $query);
+
+    if ($result) {
+    $rows=getRowData($result);
+       viewTable($rows);
+    } else {
+        echo "데이터 읽기 실패";
     }
+
+} else {
+    echo "접속 실패";
+}
+function getRowData($result) {
+     //데이터 갯수
+     $cnt = mysqli_num_rows($result);
+
+     echo "데이터 갯수는 =".$cnt."<br>";
+     $rows = [];
+
+      for($i=0; $i<$cnt; $i++){
+         $rows []= mysqli_fetch_object($result);
+         //print_r($row);
+         }
+        echo "<pre>";
+        print_r($rows);
+        return $rows;
+}
+
+
+
+ function viewTable($rows){
+    echo "<table border=1>";
+    //index 배열의 갯수를 확인해서 반복함
+    for($i=0;$i<count($rows);$i++){
+        echo "<tr>"; //열출력
+
+        //각각의 index 배열을 선택
+        //안에있는 연상배열을 반복하는 루프 반복문
+        foreach($rows[$i] as $value) {
+
+            echo "<td>".$value."</td>";//행출력
+        }
+        echo "</tr>";
+    }
+
+
+
+    echo "</table>";
+ } 
