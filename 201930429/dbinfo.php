49e0987851972d524a8b7925f421f92651bd0337
GamJarin <59593043+JangIBin@users.noreply.github.com>
2020-10-27 12:49:56 +0900

    장이빈-1027학습내용

---
+++ 201930429/dbinfo.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbinfo['master'] = [
+        'dbhost' => "localhost",
+        'dbuser' => "daelim5",
+        'dbpass' => "123456",
+        'dbschema' => "phpdaelim5",
+        'dbport' => "3306"
+    ];
