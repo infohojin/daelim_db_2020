2136832a615e31127e2f7c92e594f6ea23198818
namkoun <skarud0328@gmail.com>
2020-10-27 13:04:21 +0900

    김남경_9주차,10주차 실습퀴즈

---
+++ 201740202/class/daelimUniv.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace modules\jiny;
+
+class daelimUniv{
+    
+
+
+    public function hello($name){
+        echo $name;
+    }
+}
+$obj =new daelimUniv;
+$obj->hello("daelim");
---
+++ 201740202/class/index.php
@@ -0,0 +1,16 @@
+<?php
+$name = "대림대학교";
+
+
+
+
+//
+function hello($name){
+    echo $name;
+}
+if(!function_exists("hello")){
+function hello($name){
+    echo $name;
+}}
+hello("대림이");
+hello("대숙이");
