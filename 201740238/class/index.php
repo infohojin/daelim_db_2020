271086fc14e23dca72c9b701c71f646ae1ff523c
rx5460 <rx460@naver.com>
2020-11-03 14:13:44 +0900

    자기이름-메시지 작성

---
+++ 201740238/class/index.php
@@ -0,0 +1,18 @@
+<?php
+    //echo"대림대";
+
+    function hello($name)
+    {
+        echo $name;
+    }
+    if(!function_exists("hello")) {
+        function hello($name)
+        {
+            echo $name;
+        }    
+    }
+    $name = "univ";
+    hello("대림대학교");
+    hello("daelim");
+    
+
