7eaa4f1c6ae026abb7409a8d135c77e1bb09657f
Daegeon-kim <eorjs9706@gmail.com>
2020-11-05 20:52:38 +0900

    김대건-9.10주차 수업퀴즈

---
+++ 201740106/db/new.php
@@ -0,0 +1,45 @@
+<?php
+include "../dbinfo.php";
+$db0 = new mysqli(
+    $dbinfo['master']['dbhost'],
+    $dbinfo['master']['dbuser'],
+    $dbinfo['master']['dbpass'],
+    $dbinfo['master']['dbschema'],
+    $dbinfo['master']['dbport']
+);
+
+if ($db0) {
+    // DB접속 성공...
+    // 쿼리 스키마.테이블
+    $tablename = "instargram";
+    $title = $_POST['title'];
+    if ($title) {
+        // print_r($_POST);
+        // $query = "INSERT INTO phpdaemin5.".$tablename." (`title`) VALUES ('".$title."');";
+        $query = "INSERT phpdaemin5.".$tablename." SET ";
+        foreach($_POST as $key => $value) {
+            $query .= "`".$key."`='".$value."', ";
+        }
+        // $query .= "`title`='".$title."', ";
+        // $query .= "`description`='".$_POST['description']."', ";
+        $query .= "`regdate`='".date("Y-m-d", time())."'";
+        echo $query;
+        // exit; // 프로그램 중단
+        $result = mysqli_query($db0, $query); // DB서버로 전송
+
+        // 페이지를 이동합니다.
+        $url = "insta.php";
+        header("location:" . $url);
+    }
+
+} else {
+    echo "접속 실패";
+}
+
+echo "새글을 작성합니다.";
+if(isset($_POST['title'])) {
+    echo "작성한 새글은=".$_POST['title'];
+}
+
+$form = file_get_contents("../resource/new.html");
+echo $form;
