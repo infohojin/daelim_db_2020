db9b0ba1696a2430907ffc9778fe8de26d20f2d2
hojinlee <infohojin@gmail.com>
2020-11-24 14:59:20 +0900

    delete post

--- 000000/instagram/edit.php
+++ 000000/instagram/edit.php
@@ -93,7 +93,9 @@ $layout = file_get_contents($theme['layout']);
     }
     $form_str .= "<input type=hidden name=id value='".$_GET['id']."'>";
     $contents = str_replace("{{formlist}}", $form_str, $contents);
-        
+
+$contents = str_replace("{{id}}", $_GET['id'], $contents); // 삭제링크를 변경
+
 $layout = str_replace("{{contents}}", $contents, $layout );
 echo $layout;
 
