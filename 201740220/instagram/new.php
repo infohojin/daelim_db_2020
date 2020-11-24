eb2462c21a9bd90667dde144501a2f3353b881e0
hyeongjuSung <tjdgudwn1@gmail.com>
2020-11-24 14:08:48 +0900

    14주차_실습

--- 201740220/instagram/new.php
+++ 201740220/instagram/new.php
@@ -83,6 +83,10 @@ foreach($tableinfo as $fieldname) {
     $inputForm = file_get_contents("../resource/bootstrap/form_input.html");
     $inputForm = str_replace("{{name}}", $fieldname, $inputForm);
     $inputForm = str_replace("{{title}}", $fieldname, $inputForm);
+    
+    $inputForm = str_replace("{{value}}", "", $inputForm);
+    $inputForm = str_replace("{{description}}", "...", $inputForm);
+    
     $inputs .= $inputForm;
 
 }
