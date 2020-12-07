7fdb36ce4ab8d4cd08da5a03687db070fc3e6013
sgbo5003 <sgbo5003@naver.com>
2020-12-07 23:52:48 +0900

    박상준-14주차

---
+++ 201740212/instagram/setting.php
@@ -0,0 +1,73 @@
+<?php
+include "theme.conf.php";
+include "../dbinfo.php";
+
+include "desc.php";
+
+echo "테이블 관리 화면 입니다.";
+echo "<a href='/'>목록이동</a>";
+
+
+$db0 = new mysqli(
+    $dbinfo['master']['dbhost'],
+    $dbinfo['master']['dbuser'],
+    $dbinfo['master']['dbpass'],
+    $dbinfo['master']['dbschema'],
+    $dbinfo['master']['dbport']
+);
+
+if(isset($_GET['colum'])){
+    $query = "ALTER TABLE `phpdaelim5`.`instagram` 
+    DROP COLUMN `".$_GET['colum']."`;";
+    echo $query;
+    $result = mysqli_query($db0, $query); 
+} else
+if (isset($_GET['mode']) && $_GET['mode'] == "new" ){
+    echo "컬럼을 삽입합니다.";
+    echo "삽입=".$_POST['fieldname'];
+    
+    $query = "ALTER TABLE `phpdaelim5`.`instagram` 
+    ADD COLUMN `".$_POST['fieldname']."` VARCHAR(45) NULL;";
+    $result = mysqli_query($db0, $query);
+    
+}
+
+
+
+if($db0) {
+    $tablename = "instagram";
+    //$tableinfo = desc($db0, $tablename);
+    $query = "desc phpdaelim5.".$tablename.";";
+    $result = mysqli_query($db0, $query); // db서버로 전송
+    if($result) {
+        // echo __LINE__.": SQL 명령 실행 성공, 결과값이 있어요.<br>";
+        $fields = []; // 배열 초기화
+        echo "<table>";
+        while($row = mysqli_fetch_object($result)){
+            echo "<tr>";
+            foreach($row as $col){
+                echo "<td>".$col."</td>";
+            }
+
+            echo "<td>"."<a class=\"btn btn-warning\" href = '?colum=".$row->Field."' >삭제</a>"."</td>";
+            //print_r($row);
+            // echo "필드명 :".$row->Field."<br>";
+            //$fields []= $row->Field;
+            echo "</tr>";
+        }
+        echo "</table>";
+        //echo "<a href='?mode=new' class=\"btn btn-secondary\">추가</a>";
+
+        echo "<form action=\"?mode=new\" method='post'>";
+        echo "<input type='text' name='fieldname'>";
+        echo "<input type='submit' value='추가'>";
+        echo "</form>";
+
+    } else {
+        // echo __LINE__.": SQL 실행 오류<br>";
+    }
+    
+    //print_r($tableinfo);
+}
+
+
